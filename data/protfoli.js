00838c2c703af47ee6b86281d88890276e337531
charliedbeing <zding12@my.centennialcollege.ca>
2022-09-30 09:21:09 -0400

    first static content version done

---
+++ data/protfoli.js
@@ -0,0 +1,131 @@
+var data={
+    missions:[
+        { title:'Solve real life and business problems',
+          content:'I believe technology was driven by the requirements. In the right time, at the right place ,using the right technology to resolve problems is what I am going to excel at.'
+        },
+        { title:'I am an engineer, software craftsman, and a  part of a team.',
+ 
+         content:`Through engineering approaches, all kinds of design patterns and best practices are highly valuable to individuals and teams.
+         `
+        },
+        { title:'I am an optimist and realist in scrum context.',
+        content:`I believe when you start to make your hands dirty, almost all issues resolve over 50 percent. Step by step and continuing evaluation is king to embrace changes.
+        `
+        },
+      ],
+
+      about:[{
+        title:`Front end, Vue,React, Angular`,
+        content:
+        `
+  • Experienced on monolithic architecture applications built with Spring framework, Java Servlets, JSP, HTML/HTML5, CSS, JavaScript (ES6), Angular, 
+  
+          `
+      },
+    {  title:`Server end, Nodejs,Java Spring`,
+    content:
+        `
+  • Hands-on experience using microservices architecture application build with Spring Boot, Spring Cloud, Eureka, Consul, Hystrix, Resilience4j, Kafka, Git, Docker.
+  • Extensive knowledge and practical experience in object-oriented programming, design pattern, data structure and algorithm, and multithreading.
+  • Contributed in many project phases, ranging from requirement gathering, documentation, designing, coding, quality control to defect resolution.
+  `
+    },
+    {
+        title:`Data Layer, Oracle, Mysql, Mongodb,Redis`,
+       
+        content:
+        `
+        •  Oracle, MS SQL Server, MySQL, PostgreSQL, MongoDB, and Redis.
+        • Developed many enterprise solutions for customers in water conservancy, agriculture, land and resources, e-commerce, finance fields in agile environment.
+        • Proven expert in data modeling from conceptual and logical design to physical implementation.
+        `     
+    }
+
+    ],
+    projects:[
+      {
+        title:`GoMaterials – Montreal, Canada`,
+        content:
+        `
+•	Participated project architecture evaluation, refactored the server-side code from python to java using Spring framework to improve maintainability. 
+•	Improved performance by 200% and reduced time spent on bug fixes by 30%.
+•	Designed products, orders and logistics three module dependent database table, using MySQL, Spring Data JPA, and Hibernate improving core business data security and stability.
+•	Refactored UI code from angular 1 to angular 2 using TypeScript, bring mobile oriented & better in performance.
+•	Designed and implemented RESTful API backend, using Spring MVC. Spring Security, JWT, JSON to make service side code scalability, flexibility and portability.
+•	Integrated SendGrid Web API and Google Maps API web service to design and implement mail marketing module and delivery fee calculate module. 
+•	Designed and coded back-end microservice version and deploy to AWS, to make our application to scale and survive infrastructure issues; using Spring Boot, Spring Cloud, Eureka, Consul, Nacos, Hystrix, Resilience4j, RabbitMQ, Kafka, Git, Docker.
+
+        `   
+      },
+      {
+        title:`Autozi.com – Beijing, China`,
+       
+        content:
+        `
+•	Participated in the design and initial realization of the company's self-developed e-commerce cloud platform using Microkernel Architecture aim to achieve IaaS, PaaS, SaaS Cloud Service ability.
+•	Implemented the two core parts of the system architecture: core system and plug-in modules, using Java 8, OSGi.
+•	Gained scalability, flexibility, functional isolation, custom processing logic four benefits.
+•	Finished data modeling from conceptual and logical design to physical implementation for item order processing, item warehousing logistics process, supply chain finance, using MySQL, Power Designer.
+•	Completed six foundation Service: Hibernate Service, Redis Service, Elasticsearch Service, Mail Service, Authenticating Service, MQ Service, REST Service, using JPA, Hibernate, Redis, Elasticsearch, Apache CXF, Apache ActiveMQ, the server-side capabilities of the cloud platform are basically shaped.
+•	Updated front end technique stack to Component Architecture using Angular 1. 
+
+        `   
+      },
+      {
+        title:`YBD.com -Beijign, China`,
+       
+        content:
+        `
+        •	Designed and developed rapid development tools  using  metadata-driven development model, so users with non-technical or non-programmer skills could work on creating functionality or applications using these tools.
+        •	Development  application browser engine that consumes the metadata and renders the applications User Interface dynamically,using Java 6 , Spring ,Hiberante, Servlet, JSP, javascript, jquery,ajax,css,html, SQL Server, MySQL, Oracle, Tomcat.
+        •	Built  enterprise applications in water conservancy, agriculture, land and resources , using the rapid development tools，Received wide acclaim and gained good profits
+        
+        `   
+      }
+    ],
+    services:[
+      {
+        title:`Web Application design and develope -Javascript `,
+        content:`
+         Vue,React, Angular,Nodejs, Express, Mongodb
+        `
+      },
+      {
+        title:`Web Application design and develope - Java `,
+        content:`
+        Vue,React, Angular,Java，Spring MVC, Spring Boot, Spring Cloud,.NET,C#, SQL_SERVER, Oracle,Mysql.
+        `
+      },
+      {
+        title:`All kinds of data analysis and presentation`,
+        content:`
+         SQL,PL/SQL,Python, D3.js, Tabeau
+        `
+      }
+    ],
+    contacts:[
+      {
+        title:`LinkedIn `,
+        content:`
+         https://www.linkedin.com/in/charliedding/
+        `
+      },
+      {
+        title:`Email me `,
+        content:`
+          charliedding@gmail.com
+        `
+      },
+      {
+        title:`Leave a message here`,
+        content:`
+         Please leave your meessage here.
+         
+        `
+      }
+    ]
+      
+
+}
+
+module.exports = data
--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,53 @@
 var express = require('express');
 var router = express.Router();
+var data = require('../data/protfoli')
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('contact-me',{summary:'Welcome you to visit Charlie\'s Portolio'});
+  res.render('contact-me',
+  {
+    summary:'Welcome you to visit Charlie\'s Portolio',
+    missions:data.missions
+  }
+  );
 });
 
 router.get('/home', function(req, res, next) {
-  res.render('contact-me',{summary:'Welcome you to visit Charlie\'s Portolio'});
+  res.render('contact-me',{
+    summary:'Welcome you to visit Charlie\'s Portolio',
+    missions:data.missions
+  });
 
 });
 
 router.get('/about', function(req, res, next) {
-  res.render('home',{summary:'Charlie has many years experiencs.'});
+  res.render('about',{
+    summary:`Full Stack/Web/API Developer (Java, JavaScript)`,
+    missions:data.about
+  });
 });
 
 
 router.get('/projects', function(req, res, next) {
-  res.render('home',{summary:'Charlie has many projects experiencs.'});
+  res.render('about',{
+    summary:'Charlie has many projects experiencs.',
+    missions:data.projects
+  });
 });
 
 router.get('/services', function(req, res, next) {
-  res.render('home',{summary:'Charlie has many services technique statck.'});
+  res.render('about',{summary:'Charlie has many services technique statck.',
+   missions:data.services
+});
 });
 
 
 router.get('/contact', function(req, res, next) {
-  res.render('home',{summary:'Charlie is waiting to server you.'});
+  res.render('about',{summary:'Charlie is waiting to server you.',
+  missions:data.contacts
+});
 });
 
 module.exports = router;
